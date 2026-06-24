d3fce2292cfe6ae54987797f193dcf93bbd18452
longzheng268 <38132402+longzheng268@users.noreply.github.com>
2026-06-24 17:36:25 +0800

    add src/main/java/com/learning/service/BaseDataService.java

---
+++ src/main/java/com/learning/service/BaseDataService.java
@@ -0,0 +1,33 @@
+package com.learning.service;
+
+import com.learning.dao.SQLiteDao;
+import com.learning.model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@Service
+public class BaseDataService {
+
+    @Autowired
+    private SQLiteDao sqliteDao;
+
+    // Students
+    public void addStudent(Student s) throws SQLException { sqliteDao.insertStudent(s); }
+    public List<Student> getAllStudents() throws SQLException { return sqliteDao.getAllStudents(); }
+    public Student getStudent(String id) throws SQLException { return sqliteDao.getStudentById(id); }
+
+    // Courses
+    public void addCourse(Course c) throws SQLException { sqliteDao.insertCourse(c); }
+    public List<Course> getAllCourses() throws SQLException { return sqliteDao.getAllCourses(); }
+    public Course getCourse(String id) throws SQLException { return sqliteDao.getCourseById(id); }
+
+    // Resources
+    public void addResource(LearningResource r) throws SQLException { sqliteDao.insertResource(r); }
+    public List<LearningResource> getAllResources() throws SQLException { return sqliteDao.getAllResources(); }
+    public List<LearningResource> getResourcesByCourse(String courseId) throws SQLException {
+        return sqliteDao.getResourcesByCourse(courseId);
+    }
+}
